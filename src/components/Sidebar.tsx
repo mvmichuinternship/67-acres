65c97a44fae857516b7ad59b4f5b522fd4b4aa3c
mv <mrramidua@gmail.com>
2024-08-05 01:30:57 +0530

    frontend commit

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,90 +1,4 @@
-// import React, { useState, useEffect } from 'react';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faHome, faEye, faPlusCircle, faUser, faSignOutAlt, faArrowUp, faExchangeAlt, faRightLeft } from '@fortawesome/free-solid-svg-icons';
-// import { Link, useNavigate } from 'react-router-dom';
-// import { Tooltip as ReactTooltip } from 'react-tooltip'
-// import 'react-tooltip/dist/react-tooltip.css'
-// import { toast } from 'react-toastify';
 
-// const Sidebar = () => {
-
-//   const navigate = useNavigate()
-
-//   const [loggedIn, setLoggedIn] = useState(false);
-//   const [role, setRole] = useState("");
-
-//   useEffect(() => {
-//     const loginData = localStorage.getItem("loginData");
-//     if (loginData) {
-//       setLoggedIn(true);
-//       const parsedData = JSON.parse(loginData);
-//       const{role}=parsedData
-//       console.log(role)
-//       setRole(role);
-
-//     }
-//   }, []);
-
-//   return (
-
-//       <nav className="bg-blue-500 h-full w-[7%] p-4 fixed flex flex-col items-center">
-//         <h1 className="text-white text-2xl font-bold mb-6">67</h1>
-//         <ul className="flex-1 space-y-4">
-//           <li>
-//             <Link to="/" data-tooltip-id='sidebar-tip' data-tooltip-content="Home"  className="flex items-center text-white hover:bg-blue-400 p-2 rounded transition-colors">
-//               <FontAwesomeIcon icon={faHome} className="h-6 w-6" />
-//             </Link>
-//           </li>
-//           <li>
-//             {role === "buyer" && (
-//               <Link to="/view-properties" data-tooltip-id='sidebar-tip' data-tooltip-content="View Properties" className="flex items-center text-white hover:bg-blue-400 p-2 rounded transition-colors">
-//                 <FontAwesomeIcon icon={faEye} className="h-6 w-6" />
-//               </Link>
-//             )}
-//             {role === "seller" && (
-//               <Link to="/my-properties" data-tooltip-id='sidebar-tip' data-tooltip-content="My properties" className="flex items-center text-white hover:bg-blue-400 p-2 rounded transition-colors">
-//                 <FontAwesomeIcon icon={faEye} className="h-6 w-6" />
-//               </Link>
-//             )}
-//           </li>
-//           <li>
-//             {role === "seller" && (
-//               <Link to="/post-property" data-tooltip-id='sidebar-tip' data-tooltip-content="Post property" className="flex items-center text-white hover:bg-blue-400 p-2 rounded transition-colors">
-//                 <FontAwesomeIcon icon={faPlusCircle} className="h-6 w-6" />
-//               </Link>
-//             )}
-//             {role === "buyer"  && (
-//               <Link to="/switch-roles" data-tooltip-id='sidebar-tip' data-tooltip-content="Switch Role" className="flex items-center text-white hover:bg-blue-400 p-2 rounded transition-colors">
-//                 <FontAwesomeIcon icon={faRightLeft} className="h-6 w-6" />
-//               </Link>
-//             )}
-//             {role === "seller"  && (
-//               <Link to="/switch-roles" data-tooltip-id='sidebar-tip' data-tooltip-content="Switch Role" className="flex items-center text-white hover:bg-blue-400 p-2 rounded transition-colors">
-//                 <FontAwesomeIcon icon={faRightLeft} className="h-6 w-6" />
-//               </Link>
-//             )}
-//           </li>
-//           <li>{loggedIn&&(
-
-//             <button onClick={()=>{localStorage.removeItem("loginData"); toast.success("Logged out!"); navigate('/login'); window.location.reload()}} data-tooltip-id='sidebar-tip' data-tooltip-content="Log out" className="flex items-center text-white hover:bg-blue-400 p-2 rounded transition-colors">
-//               <FontAwesomeIcon icon={faSignOutAlt} className="h-6 w-6" />
-//             </button>
-//           )
-//             }
-//           </li>
-//           <li>
-//             <Link to="/upgrade" data-tooltip-id='sidebar-tip' data-tooltip-content="Upgrade Plan" className="flex items-center text-white hover:bg-blue-400 p-2 rounded transition-colors">
-//               <FontAwesomeIcon icon={faArrowUp} className="h-6 w-6" />
-//             </Link>
-//           </li>
-//         </ul>
-//         <ReactTooltip id='sidebar-tip' />
-//       </nav>
-
-//   );
-// };
-
-// export default Sidebar;
 
 import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -99,7 +13,7 @@ import {
   faCoins
 } from "@fortawesome/free-solid-svg-icons";
 import { Link, useNavigate } from "react-router-dom";
-import { Tooltip as ReactTooltip } from "react-tooltip";
+import Tooltip from "react-tooltip";
 import "react-tooltip/dist/react-tooltip.css";
 import { toast } from "react-toastify";
 import SwitchRole from "../screens/SwitchRoles.tsx";
@@ -263,7 +177,7 @@ const Sidebar = () => {
         </li>
         )}
       </ul>
-      <ReactTooltip id="sidebar-tip" />
+      <Tooltip id="sidebar-tip" />
     </nav>
   );
 };
